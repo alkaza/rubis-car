bead820e577ff51abee78d3ebfcb100c1c6f62ca
alkaza <alenadanse@gmail.com>
2017-12-19 15:20:08 +0900

    update firmware

--- Arduino/Firmware/Drive/Drive.ino
+++ Arduino/Firmware/Drive/Drive.ino
@@ -1,16 +1,28 @@
+#include <Servo.h>
+#include <RobotEQ.h>
 #include <ros.h>
 #include <std_msgs/Bool.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/Empty.h>
 #include <race/drive_values.h>
+
+#define CHANNEL_1 1
+
 ros::NodeHandle  nh;
 
-
 boolean flagStop = false;
-int pwm_center_value = 9830;  //  15% duty cycle
-int pwm_lowerlimit = 6554;    //  10% duty cycle
-int pwm_upperlimit = 13108;   //  20% duty cycle
+
+int pwm_angle_center_value = 90;  //  15% duty cycle
+int pwm_angle_lowerlimit = 30;    //  10% duty cycle
+int pwm_angle_upperlimit = 150;   //  20% duty cycle
+
+int pwm_drive_center_value = 0;  //  15% duty cycle
+int pwm_drive_lowerlimit = -1000;    //  10% duty cycle
+int pwm_drive_upperlimit = 1000;   //  20% duty cycle
+
+Servo steering;
+RobotEQ controller(&Serial3);
 
 std_msgs::Int32 str_msg;
 ros::Publisher chatter("chatter", &str_msg);
@@ -18,60 +30,51 @@ ros::Publisher chatter("chatter", &str_msg);
 int kill_pin = 2;
 unsigned long duration = 0;
 
-void messageDrive( const race::drive_values& pwm )
-{
-//  Serial.print("Pwm drive : ");
-//  Serial.println(pwm.pwm_drive);
-//  Serial.print("Pwm angle : ");
-//  Serial.println(pwm.pwm_angle);
-  
-  if(flagStop == false)
-  {
-    str_msg.data = pwm.pwm_drive;
-    chatter.publish( &str_msg );
+void messageDrive( const race::drive_values& pwm ) {
+  //  Serial.print("Pwm drive : ");
+  //  Serial.println(pwm.pwm_drive);
+  //  Serial.print("Pwm angle : ");
+  //  Serial.println(pwm.pwm_angle);
 
-    if(pwm.pwm_drive < pwm_lowerlimit)
-    {
-      analogWrite(5,pwm_lowerlimit);    //  Safety lower limit        
-    }
-    else if(pwm.pwm_drive > pwm_upperlimit)
-    {
-      analogWrite(5,pwm_upperlimit);    //  Safety upper limit
-    }
-    else
-    {
-      analogWrite(5,pwm.pwm_drive);     //  Incoming data                    
-    }
+  if (controller.isConnected()) {
+    if (flagStop == false) {
+      str_msg.data = pwm.pwm_drive;
+      chatter.publish( &str_msg );
 
-    
-    if(pwm.pwm_angle < pwm_lowerlimit)
-    {
-      analogWrite(6,pwm_lowerlimit);    //  Safety lower limit        
-    }
-    else if(pwm.pwm_angle > pwm_upperlimit)
-    {
-      analogWrite(6,pwm_upperlimit);    //  Safety upper limit
-    }
-    else
-    {
-      analogWrite(6,pwm.pwm_angle);     //  Incoming data                    
-    }
+      if (pwm.pwm_drive < pwm_drive_lowerlimit) {
+        controller.commandMotorPower(CHANNEL_1, pwm_drive_lowerlimit);    //  Safety lower limit
+      }
+      else if (pwm.pwm_drive > pwm_drive_upperlimit) {
+        controller.commandMotorPower(CHANNEL_1, pwm_drive_upperlimit);	//  Safety upper limit
+      }
+      else {
+        controller.commandMotorPower(CHANNEL_1, pwm.pwm_drive);	//  Incoming data
+      }
 
-  }
-  else
-  {
-    analogWrite(5,pwm_center_value);
-    analogWrite(6,pwm_center_value);    
+
+      if (pwm.pwm_angle < pwm_angle_lowerlimit) {
+        steering.write(pwm_angle_lowerlimit);    //  Safety lower limit
+      }
+      else if (pwm.pwm_angle > pwm_angle_upperlimit) {
+        steering.write(pwm_angle_upperlimit);    //  Safety upper limit
+      }
+      else {
+        steering.write(pwm.pwm_angle);	//  Incoming data
+      }
+
+    }
+    else {
+      controller.commandMotorPower(CHANNEL_1, pwm_drive_center_value);
+      steering.write(pwm_angle_center_value);
+    }
   }
 }
 
-void messageEmergencyStop( const std_msgs::Bool& flag )
-{
+void messageEmergencyStop( const std_msgs::Bool& flag ) {
   flagStop = flag.data;
-  if(flagStop == true)
-  {
-    analogWrite(5,pwm_center_value);
-    analogWrite(6,pwm_center_value);    
+  if ((flagStop == true && (controller.isConnected()) {
+    controller.commandMotorPower(CHANNEL_1, pwm_drive_center_value);
+    steering.write(pwm_angle_center_value);
   }
 }
 
@@ -81,16 +84,18 @@ ros::Subscriber<std_msgs::Bool> sub_stop("eStop", &messageEmergencyStop );
 
 
 void setup() {
-  
-  analogWriteFrequency(5, 100);
-  analogWriteFrequency(6, 100);
+  Serial3.begin(115200);
+
+  if (controller.isConnected()) {
+    controller.commandMotorPower(CHANNEL_1, pwm_drive_center_value);
+    steering.write(pwm_angle_center_value);
+  }
+
   analogWriteResolution(16);
-  analogWrite(5,pwm_center_value);
-  analogWrite(6,pwm_center_value);
-  pinMode(13,OUTPUT); 
-  digitalWrite(13,HIGH);
-  pinMode(2,INPUT); 
-//  digitalWrite(2,LOW);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  pinMode(2, INPUT);
+  //  digitalWrite(2,LOW);
 
   nh.initNode();
   nh.subscribe(sub_drive);
@@ -102,29 +107,29 @@ void setup() {
 void loop() {
   nh.spinOnce();
   duration = pulseIn(kill_pin, HIGH, 30000);
-  while(duration > 1900)
-  {
+
+  while ((duration > 1900) && (controller.isConnected()) {
     duration = pulseIn(kill_pin, HIGH, 30000);
-    analogWrite(5,pwm_center_value);
-    analogWrite(6,pwm_center_value);        
+    controller.commandMotorPower(CHANNEL_1, pwm_drive_center_value);
+    steering.write(pwm_angle_center_value);
   }
-  // put your main code here, to run repeatedly:
+
   /*
-  if(Serial.available())
-  {
+    if(Serial.available() && (controller.isConnected())
+    {
     int spd = Serial.read();
     if(spd>127) {
       spd = spd-128;
       spd = map(spd,0,100,410,820);
-      analogWrite(5,spd);  
+      controller.commandMotorPower(CHANNEL_1, spd);
     }
     else {
       //angle servo
       spd = map(spd,0,100,410,820);
-      analogWrite(6,spd);
+      steering.write(spd);
     }
-    
-  } 
-  */ 
+
+    }
+  */
 }
 
